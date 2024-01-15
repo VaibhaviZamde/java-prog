8df0be5ced1ca7ed10cd2dc39a4bd2e914daa7ec
VaibhaviZamde <2023bit503@sggs.ac.in>
2024-01-16 00:52:33 +0530

    WAP to print 1 to 5 no. using while loop

---
+++ Print.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Print 
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		int i = 1;
+		System.out.println("1 to 5 no. are ");
+		
+		while(i<=5)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
