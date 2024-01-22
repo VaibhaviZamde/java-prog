cd946cc2ff2cf00df698d8c788839a762b1cce76
VaibhaviZamde <2023bit503@sggs.ac.in>
2024-01-23 00:31:10 +0530

    For each loop program which is used to access data from collection

---
+++ Foreach.java
@@ -0,0 +1,12 @@
+class Foreach
+{
+	public static void main(String args[])
+	{
+		String arr[] = {"Vaibhvai","Mohit","Sayali","Shruti"};
+		
+		for(String x : arr)
+		{
+			System.out.println("x = "+x);
+		}
+	}
+}
