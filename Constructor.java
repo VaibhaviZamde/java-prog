04a1518ac49bf00b44d51ec50f35a4e4bebf29d7
VaibhaviZamde <2023bit503@sggs.ac.in>
2024-02-09 01:52:56 +0530

    COnstructor program

---
+++ Constructor.java
@@ -0,0 +1,20 @@
+class Constructor
+{
+	
+	public static void main(String args[])
+	{
+		Constructor cns = new Constructor(10);
+		//System.out.println(cns);
+	}
+	
+	public Constructor()
+	{
+		int x = 2634;
+		System.out.println(x);
+	}
+	
+	public Constructor(int x)
+	{
+		System.out.println(x);
+	}
+}
