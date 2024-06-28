9ad83a3fa8139c4d8ff63be0aea26cb0cd6ab0e9
VaibhaviZamde <2023bit503@sggs.ac.in>
2024-06-28 22:55:03 +0530

    Taking stamdard input in java using Scanner class

---
+++ StandardInput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class StandardInput
+{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter any number :");
+		int n = s.nextInt();
+		
+		System.out.println("Number is = "+n);
+	}
+}
